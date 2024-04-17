2883c60cec2574c3e367bcf864cd639c9e4bfd2a
Dylan <113315259+layzboie@users.noreply.github.com>
2024-04-17 16:00:40 -0400

    Lane with enum

--- Lane.java
+++ Lane.java
@@ -1,12 +1,19 @@
 package Highway;
 
 import java.util.List;
+import java.util.Queue;
 
 public class Lane {
 
     int laneNumber;
+
+    enum congestionLevel{
+        notCongested,
+        moderateCongestion,
+        highlyCongested;
+    }
+
     public Lane(int laneNumber){
         this.laneNumber = laneNumber;
     }
-
 }
