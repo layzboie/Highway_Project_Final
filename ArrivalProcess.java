752b2091a9191771635f2f0aca50cde2f5ebfdd3
Tyler <tyler.vierbuchen@comcast.net>
2024-04-18 20:27:19 -0400

    changed ArrivalProcess (not finished)

--- ArrivalProcess.java
+++ ArrivalProcess.java
@@ -1,21 +1,26 @@
-package Highway;
-
+package Highway; //what is this? what does it do?
 public class ArrivalProcess {
     NormalDistribution arrivalDistribution = new NormalDistribution();
+//add cars will be used to add cars to the lanes
+    //add cars will take in a mean and standard deviation
+public void addCars(double mean, double standardDeviation){
+    arrivalDistribution.setMean(mean);
+    arrivalDistribution.setStandardDeviation(standardDeviation);
 
-    public int time = 0;//placeholder for the clock
+    //rounds the generated number so a full (non-decimal) number of cars is added
+    double carNumber = Math.round(arrivalDistribution.sample());
 
-    public double nextArrivalTime;
-public Car nextCar(Exit[] allExits){
-    //sets up how often cars will enter the road
-    arrivalDistribution.setMean(5);
-    arrivalDistribution.setStandardDeviation(2);
+    int i = 0;//loop variable
+//loop to add a certain number of cars to a lane
+    while (i < carNumber){
+        Car car;
+        //will need some way to enqueue cars to all lanes in a section
+        //and for every section. May want to call add cars for every lane
+        //For example: rightLane.addCars(mean, standardDeviation); (and so on)
+        //Will want to do this for every section
+        i++;
+    }
 
-    nextArrivalTime = time + arrivalDistribution.sample();
-    //creates a car everytime nextArrival time is called
-    Car car = new Car(allExits);
-    car.arrivalTime = nextArrivalTime;
-    return car;
 }
 
 }
