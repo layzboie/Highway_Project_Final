c79b2ee43b5260884ee7f3f238ecf83d8ef67dc3
Tyler <tyler.vierbuchen@comcast.net>
2024-04-23 14:30:15 -0400

    The addCars method has been added to the lane class.

--- Lane.java
+++ Lane.java
@@ -1,9 +1,9 @@
 package Highway;
 
-import java.util.List;
-import java.util.Queue;
+import static Highway.Highway.allExits;
 
-public class Lane extends Highway.Queue {
+public class Lane {
+    Queue lane = new Queue();
 
     int laneNumber;
 
@@ -15,6 +15,27 @@ public class Lane extends Highway.Queue {
 
     public Lane(int laneNumber){
         this.laneNumber = laneNumber;
+    }
+
+    NormalDistribution arrivalDistribution = new NormalDistribution();
+    //add cars will be used to add cars to the lanes
+    //add cars will take in a mean and standard deviation
+    public void addCars(double mean, double standardDeviation){
+        arrivalDistribution.setMean(mean);
+        arrivalDistribution.setStandardDeviation(standardDeviation);
+
+        //rounds the generated number so a full (non-decimal) number of cars is added
+        double carNumber = Math.round(arrivalDistribution.sample());
+
+        int i = 0;//loop variable
+//loop to add a certain number of cars to a lane
+        //this should work, but i could not figure out how to test it
+        while (i < carNumber){
+            Car car = new Car(allExits); //creates a car
+            lane.enqueue(car); //adds cars to the lane
+            i++; //used to make sure loop eventually ends
+        }
 
     }
+
 }
